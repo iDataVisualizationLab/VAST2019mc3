3d0db9e42615914d99803eead44b1fb980b8fdbe
nnhuyen <huyen.nguyen@ttu.edu>
2019-07-02 21:26:33 -0500

    Multiple color for detail box

--- js/detailOnDemand.js
+++ js/detailOnDemand.js
@@ -1,5 +1,5 @@
-function tooltipInfo(d, wsRawData){
-    if (d.topic === "location"){
+function tooltipInfo(d, wsRawData) {
+    if (d.topic === "location") {
         let limited = wsRawData
             .slice(
                 bisect(wsRawData, +d.time),
@@ -20,90 +20,61 @@ function tooltipInfo(d, wsRawData){
     }
 }
 
-function createTableTooltip(wsTooltipDiv, info, text, prevColor, topic){
+function createTableTooltip(wsTooltipDiv, info, text, prevColor, topic) {
     wsTooltipDiv.selectAll("*").remove();
     // process info text
+    let table = wsTooltipDiv.append("table")
+            .attr("class", "tableTooltip")
+            .attr("id", "tableTooltip")
+            .style("width", "100%"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
 
-    if (topic === "location"){
-        let rows = createTable();
-        let cells = rows.selectAll("td")
-            .data(function (row) {
-                return columns.map(function (column) {
-                    return {column: column, value: row[column]}
-                })
+    // header row
+    thead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+        .attr("class", column => "column-" + column)
+        .text(column => capitalize(column));
+
+    // create a row for each record
+    let rows = tbody.selectAll("tr")
+        .data(info)
+        .enter()
+        .append("tr");
+
+    let cells = rows.selectAll("td")
+        .data(function (row) {
+            return columns.map(function (column) {
+                return {column: column, value: row[column]}
             })
-            .enter()
-            .append("td")
-            .text(d => d.column === "time"? formatTimeDetailBox(d.value):d.value)
-            .style("color", d => d.column === "location"? topicColor[1] : "#000");
-    }
-    else {
-        info.forEach(d => {
-            d.messageArr = d.message.toLowerCase().split(text);
-            d.messageArr.splice(1,0,text);
-        });
-
-        let rows = createTable();
-
-        // create a cell in each row, for each column
-        let cells = rows.selectAll("td")
-            .data(function (row) {
-                return columns.map(function (column) {
-                    return column === "message" ?
-                        {column: column, value: row[column], array: row["messageArr"]} :
-                        {column: column, value: row[column]}
-                })
-            })
-            .enter()
-            .append("td")
-            .attr("class", cell => "cell" + cell.column)
-            .text(d => {
-                if (d.column === "time"){
-                    return formatTimeDetailBox(d.value)
+        })
+        .enter()
+        .append("td")
+        .style("color", d => ((d.column === "location") && (topic === "location")) ? topicColor[1] : "#000")
+        .html(function (d) {
+            if (d.column === "time") {
+                return formatTimeDetailBox(d.value)
+            }
+            else if (d.column === "message") {
+                let sentence = d.value;
+                for (let i = 0; i < dataOption.length; i++) {
+                    for (let j = 0; j < dataOption[i].content.length; j++) {
+                        let index = sentence.toLowerCase().indexOf(dataOption[i].content[j]);
+                        if (index >= 0) {
+                            let word = sentence.slice(index).split(/\W/)[0];
+                            let replaceWord = `<span style="color: ${dataOption[i].color}"` + ">" + word + "</span>";
+                            sentence = name(sentence, word, replaceWord);
+                        }
+                    }
                 }
-                else if (d.column === "message"){
-                    return ""
-                }
-                else return d.value;
-            });
-
-        rows.selectAll(".cellmessage")
-            .selectAll("span")
-            .data(function (d) {
-                console.log(d);
-                return d.array;
-            })
-            .enter()
-            .append("span")
-            .text(d => d)
-            .style("color", d => d.trim() === text ? prevColor : "#000");
-    }
-
-    function createTable(){
-        let table = wsTooltipDiv.append("table")
-                .attr("class", "tableTooltip")
-                .attr("id", "tableTooltip")
-                .style("width", "100%"),
-            thead = table.append("thead"),
-            tbody = table.append("tbody");
-
-        // header row
-        thead.append("tr")
-            .selectAll("th")
-            .data(columns)
-            .enter()
-            .append("th")
-            .attr("class", column => "column-" + column)
-            .text(column => capitalize(column));
-
-        // create a row for each record
-        let rows = tbody.selectAll("tr")
-            .data(info)
-            .enter()
-            .append("tr");
-
-        return rows
-    }
+                return sentence;
+            }
+            else return d.value;
+        })
+    ;
 }
 
 function highlight(info, wsData, timestep) {
@@ -131,11 +102,16 @@ function highlight(info, wsData, timestep) {
     topics.forEach(topic => {
         thisColumn.words[topic]
             .filter(d => {
-            return ((d.placed) && (eval(topic + "Info").indexOf(d.text) >= 0))
-        })
+                return ((d.placed) && (eval(topic + "Info").indexOf(d.text) >= 0))
+            })
             .forEach(d => {
                 d3.select("#" + removeChar(d.text) + timestep)
                     .classed("highlightText", true);
             })
     })
+}
+
+function name(str, replaceWhat, replaceTo) {
+    var re = new RegExp(replaceWhat, 'g');
+    return str.replace(re, replaceTo);
 }
